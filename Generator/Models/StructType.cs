f4935914b1ea3df599fcca9c584f3b632c2215a2
Gregory Brutsche <gregory@mail.brutsches.local>
2025-10-05 02:24:19 -0400

    Fix MHWS EffectManagerSetting EnemyID not being detected as button binding.

--- Generator/Models/StructType.cs
+++ Generator/Models/StructType.cs
@@ -130,6 +130,7 @@ public class StructType(string name, string? parent, uint hash, StructJson struc
             case "App_MissionIDList_ID_Serializable.Value": return DataSourceType.QUESTS;
             case "App_NpcDef_ID_Serializable.Value": return DataSourceType.NPCS;
             case "App_OtEquipDef_EQUIP_DATA_ID_Serializable.Value": return DataSourceType.OTOMO_SERIES;
+            case "App_user_data_EffectManagerSetting_cHitmarkColor.EnemyID_Fixed": return DataSourceType.ENEMIES;
             case "App_WeaponCharmDef_TYPE_Serializable.Value": return DataSourceType.PENDANTS;
             case "App_WeaponDef_SERIES_Serializable.Value": return DataSourceType.WEAPON_SERIES;
 #endif
