b638ddcee78313274225468b4062a22f61949044
Gregory Brutsche <gregory@mail.brutsches.local>
2025-10-10 18:49:39 -0400

    Add rapid-fire options to the MHWS bowgun mod.

--- RE-Editor/Mods/MHWS/AllAmmoForBowguns.cs
+++ RE-Editor/Mods/MHWS/AllAmmoForBowguns.cs
@@ -17,14 +17,10 @@ namespace RE_Editor.Mods;
 [UsedImplicitly]
 public class AllAmmoForBowguns : IMod {
     [UsedImplicitly]
+    [SuppressMessage("ReSharper", "GrammarMistakeInComment")]
     public static void Make(MainWindow mainWindow) {
-        const string name                   = "BG Ammo Tweaks"; // All Ammo for LBG & HBG
-        const string descriptionSl1         = "All BG can use all ammo; existing shelling levels unchanged.";
-        const string descriptionSl3         = "All BG can use all ammo; all shelling levels changed to the highest shell level available for that ammo.";
-        const string descriptionAmmoCapHalf = "All added ammo is set to half the highest capacity (rounding up) for that ammo type across compatible weapons.";
-        const string descriptionAmmoCapHigh = "All added ammo is set to the highest capacity for that ammo type across compatible weapons.";
-        const string descriptionAmmoCap15   = "All ammo is set to 15.";
-        const string version                = "1.0";
+        const string name    = "BG Ammo Tweaks"; // All Ammo for LBG & HBG
+        const string version = "1.1";
 
         var baseLuaMod = new VariousDataTweak {
             Version      = version,
@@ -36,6 +32,7 @@ public class AllAmmoForBowguns : IMod {
         var highestSl  = new App_BowgunShellLv_SHELL_LV_Fixed[20];
         var highestCap = new short[20];
 
+        // ReSharper disable once ForeachCanBePartlyConvertedToQueryUsingAnotherGetEnumerator
         foreach (var weaponName in VariousDataWriter.WEAPON_TYPES_GUNS_ONLY) {
             var file       = $@"{PathHelper.CHUNK_PATH}\natives\STM\GameDesign\Common\Weapon\{weaponName}.user.3";
             var weaponData = ReDataFile.Read(file).rsz.GetEntryObject<App_user_data_WeaponData>().Values.Cast<App_user_data_WeaponData_cData>().ToList();
@@ -47,6 +44,7 @@ public class AllAmmoForBowguns : IMod {
                 }
                 for (var i = 0; i < data.ShellNum.Count; i++) {
                     highestCap[i] = Math.Max(highestCap[i], data.ShellNum[i].Value);
+                    highestCap[i] = Math.Max(highestCap[i], data.RapidShellNum[i].Value);
                 }
             }
         }
@@ -61,17 +59,18 @@ public class AllAmmoForBowguns : IMod {
                 _ => throw new ArgumentOutOfRangeException(nameof(weaponName), weaponName, null)
             };
 
+            // Shell Level
             foreach (var levelMode in Enum.GetValues<ShellLevelMode>()) {
                 string shortName;
                 string desc;
                 switch (levelMode) {
                     case ShellLevelMode.PRESERVE_LOWEST:
                         shortName = "Existing SL Unchanged";
-                        desc      = descriptionSl1;
+                        desc      = "All BG can use all ammo; existing shelling levels unchanged.";
                         break;
                     case ShellLevelMode.SL3:
                         shortName = "All SL Maxed";
-                        desc      = descriptionSl3;
+                        desc      = "All BG can use all ammo; all shelling levels changed to the highest shell level available for that ammo.";
                         break;
                     default: throw new ArgumentOutOfRangeException(nameof(levelMode), levelMode, null);
                 }
@@ -109,21 +108,22 @@ public class AllAmmoForBowguns : IMod {
                          .SetSkipPak(true));
             }
 
+            // Ammo Count
             foreach (var ammoMode in Enum.GetValues<ShellAmmoMode>()) {
                 string shortName;
                 string desc;
                 switch (ammoMode) {
                     case ShellAmmoMode.HALF:
                         shortName = "Half Ammo Cap";
-                        desc      = descriptionAmmoCapHalf;
+                        desc      = "All added ammo is set to half the highest capacity (rounding up) for that ammo type across compatible weapons.";
                         break;
                     case ShellAmmoMode.MAX:
                         shortName = "Max Ammo Cap";
-                        desc      = descriptionAmmoCapHigh;
+                        desc      = "All added ammo is set to the highest capacity for that ammo type across compatible weapons.";
                         break;
                     case ShellAmmoMode.NUM_15:
                         shortName = "15 Ammo Cap";
-                        desc      = descriptionAmmoCap15;
+                        desc      = "All ammo is set to 15.";
                         break;
                     default: throw new ArgumentOutOfRangeException(nameof(ammoMode), ammoMode, null);
                 }
@@ -159,7 +159,62 @@ public class AllAmmoForBowguns : IMod {
                          .SetChanges([
                              new() {
                                  Target = target,
-                                 Action = writer => ModShellAmmoRef(writer, weaponName, capChangesMap)
+                                 Action = writer => ModShellAmmoRef(writer, weaponName, capChangesMap, ShellAmmoTarget.NORMAL)
+                             }
+                         ])
+                         .SetSkipPak(true));
+            }
+
+            // LBG Rapid Fire
+            foreach (var ammoMode in Enum.GetValues<ShellAmmoMode>()) {
+                if (weaponName == "HeavyBowgun") continue;
+
+                string shortName;
+                string desc;
+                switch (ammoMode) {
+                    case ShellAmmoMode.HALF:
+                        shortName = "Rapid Fire - Half Ammo Cap";
+                        desc      = "All added ammo is set to half the highest capacity (rounding up) for that ammo type across compatible weapons.";
+                        break;
+                    case ShellAmmoMode.MAX:
+                        shortName = "Rapid Fire - Max Ammo Cap";
+                        desc      = "All added ammo is set to the highest capacity for that ammo type across compatible weapons.";
+                        break;
+                    case ShellAmmoMode.NUM_15:
+                        shortName = "Rapid Fire - 15 Ammo Cap";
+                        desc      = "All ammo is set to 15.";
+                        break;
+                    default: throw new ArgumentOutOfRangeException(nameof(ammoMode), ammoMode, null);
+                }
+
+                Dictionary<int, Dictionary<int, short>> capChangesMap = [];
+
+                foreach (var data in weaponData) {
+                    var bgId = (int) data.LightBowgun;
+
+                    for (var i = 0; i < data.RapidShellNum.Count; i++) {
+                        if (highestCap[i] == 0) throw new("Highest ammo cap entry is zero!");
+
+                        if (data.RapidShellNum[i].Value == 0 || ammoMode == ShellAmmoMode.NUM_15) {
+                            if (!capChangesMap.ContainsKey(bgId)) capChangesMap[bgId] = [];
+                            capChangesMap[bgId][i] = ammoMode switch {
+                                ShellAmmoMode.HALF => (short) (Math.Ceiling(highestCap[i] / 2f)),
+                                ShellAmmoMode.MAX => highestCap[i],
+                                ShellAmmoMode.NUM_15 => 15,
+                                _ => throw new ArgumentOutOfRangeException(nameof(ammoMode), ammoMode, null)
+                            };
+                        }
+                    }
+                }
+
+                mods.Add(baseLuaMod
+                         .SetName($"{name} - {shortName} ({weaponName})")
+                         .SetDesc(desc)
+                         .SetDefaultLuaName()
+                         .SetChanges([
+                             new() {
+                                 Target = target,
+                                 Action = writer => ModShellAmmoRef(writer, weaponName, capChangesMap, ShellAmmoTarget.RAPID)
                              }
                          ])
                          .SetSkipPak(true));
@@ -182,11 +237,19 @@ public class AllAmmoForBowguns : IMod {
     }
 
     [SuppressMessage("ReSharper", "StringLiteralTypo")]
-    public static void ModShellAmmoRef(StreamWriter writer, string weaponName, Dictionary<int, Dictionary<int, short>> ammoChangesMap) {
+    public static void ModShellAmmoRef(StreamWriter writer, string weaponName, Dictionary<int, Dictionary<int, short>> ammoChangesMap, ShellAmmoTarget target) {
         foreach (var (bgId, changes) in ammoChangesMap) {
             writer.WriteLine($"    if (entry._{weaponName} == {bgId}) then");
             foreach (var (index, newCap) in changes) {
-                writer.WriteLine($"        entry._{nameof(App_user_data_WeaponData_cData.ShellNum)}[{index}] = {newCap}");
+                switch (target) {
+                    case ShellAmmoTarget.NORMAL:
+                        writer.WriteLine($"        entry._{nameof(App_user_data_WeaponData_cData.ShellNum)}[{index}] = {newCap}");
+                        break;
+                    case ShellAmmoTarget.RAPID:
+                        writer.WriteLine($"        entry._{nameof(App_user_data_WeaponData_cData.RapidShellNum)}[{index}] = {newCap}");
+                        break;
+                    default: throw new ArgumentOutOfRangeException(nameof(target), target, null);
+                }
             }
             writer.WriteLine("        entry:call(\"onLoad\")");
             writer.WriteLine("    end");
@@ -202,5 +265,10 @@ public class AllAmmoForBowguns : IMod {
         HALF,
         MAX,
         NUM_15
+    }
+
+    public enum ShellAmmoTarget {
+        NORMAL,
+        RAPID
     }
 }
